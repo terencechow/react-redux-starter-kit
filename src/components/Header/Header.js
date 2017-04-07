2f06e5b1968659457439d81ab78a571dbe074635
Terence Chow <terencechow@quillcontent.com>
2017-04-07 10:47:48 +0100

    add nav to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,10 @@ export const Header = () => (
     <Link to='/counter' activeClassName='route--active'>
       Counter
     </Link>
+    {' · '}
+    <Link to='/otherPage' activeClassName='route--active'>
+      Other Page
+    </Link>
   </div>
 )
 
